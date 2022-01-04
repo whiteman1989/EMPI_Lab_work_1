091b0e5e9cbb811a0720cabfbb48dda7c488b700
AndreyGorban <Whiteman1989@gmail.com>
2022-01-04 19:08:27 +0200

    added lab work  2

--- EMPI_Lab_work_1/Program.cs
+++ EMPI_Lab_work_1/Program.cs
@@ -3,6 +3,10 @@ sample.PrintSampleData();
 sample.PrintVariationSeries();
 sample.PrintFrequency();
 sample.PrintIntegralFrequency();
+sample.PrintMode();
+sample.PrintAvg();
+sample.PrintMedian();
+
 
 public class Sample
 {
@@ -65,6 +69,40 @@ public class Sample
                 Variant = group.Key,
                 Frequency = (feqSum += group.Count())
             }).ToList().ForEach(f => Console.WriteLine(f));
+    }
+
+    public void PrintMode()
+    {
+        Console.WriteLine("Mode:");
+        var mode = _numsInSample.GroupBy(n => n)
+        .Select(group => new
+        {
+            Variant = group.Key,
+            Frequency = group.Count()
+        }).MaxBy(f => f.Frequency);
+        Console.WriteLine(mode);
+    }
+
+    public void PrintAvg()
+    {
+        Console.WriteLine("AVG:");
+        Console.WriteLine(_numsInSample.Average());
+    }
+
+    public void PrintMedian()
+    {
+        decimal median = 0m;
+        Console.WriteLine("Median:");
+        if (_numsInSample.Count % 2 != 0)
+        {
+            median = ((decimal)_numsInSample.ElementAt(_numsInSample.Count / 2 - 1) +
+                (decimal)_numsInSample.ElementAt(_numsInSample.Count / 2))/2;
+        }
+        else
+        {
+            median = _numsInSample.ElementAt(_numsInSample.Count / 2 - 1);
+        }
+        Console.WriteLine(median);
     }
 
     #endregion
